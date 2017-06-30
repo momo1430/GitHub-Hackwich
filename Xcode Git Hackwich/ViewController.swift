b89d1eba7501fc1efba7c19902604c65ddfe0f44
Shane Olson <momo1430@Shanes-MacBook-Pro.local>
2017-06-30 10:40:17 -0500

    Initial Commit

---
+++ Xcode Git Hackwich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode Git Hackwich
+//
+//  Created by Shane Olson on 6/30/17.
+//  Copyright © 2017 Shane Olson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
