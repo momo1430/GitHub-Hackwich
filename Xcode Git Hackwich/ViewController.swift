15c603ebdac1287d69feafbd005018b34e75d863
Shane Olson <momo1430@Shanes-MacBook-Pro.local>
2017-06-30 11:30:00 -0500

    mac n cheese

--- Xcode Git Hackwich/ViewController.swift
+++ Xcode Git Hackwich/ViewController.swift
@@ -8,23 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController
-{
+class ViewController: UIViewController {
 
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning()
-    {
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-
-    }
-    @IBAction func onButtonTapped(_ sender: Any)
-    {
-        print("Button was tapped")
+        // Dispose of any resources that can be recreated.
     }
 
 
